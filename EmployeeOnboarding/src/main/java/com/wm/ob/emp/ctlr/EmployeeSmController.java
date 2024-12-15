4383a2a18c6dfa909cc34c413410dc5563ba3f05
Rishi Raj <rishiraj88+github@gmail.com>
2024-12-15 11:32:02 +0100

    Update API endpoints.

--- EmployeeOnboarding/src/main/java/com/wm/ob/emp/ctlr/EmployeeSmController.java
+++ EmployeeOnboarding/src/main/java/com/wm/ob/emp/ctlr/EmployeeSmController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("eob/")
+@RequestMapping("eob/api/")
 public class EmployeeSmController {
     @Autowired
     private EmployeeSmService employeeSmService;
